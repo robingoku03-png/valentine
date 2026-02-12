dddfdd0de046aa80de80760ac3eed9bf56723f63
robingoku03-png <robingoku03@gmail.com>
2026-02-12 14:25:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,14 +11,7 @@ const teaseTexts = [
   "That’s kinda mean 😭",
   "Okay now you are pissing me😒"
 ];
-function setVH(){
-  document.documentElement.style.setProperty(
-    '--vh',
-    `${window.innerHeight * 0.01}px`
-  );
-}
-setVH();
-window.addEventListener('resize', setVH);
+
 function hearts(type,count=15){
   for(let i=0;i<count;i++){
     const h=document.createElement("div");
